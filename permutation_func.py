3119e5643bc52bf42cefb0b95739aca6c70dcebd
kerorin kitakitsune <66837061+KerorinNorthFox@users.noreply.github.com>
2021-07-14 15:29:05 +0900

    Update permutation_func.py

--- permutation_func.py
+++ permutation_func.py
@@ -1,1 +1,15 @@
 def permutation(n, r):
+  a = n - r
+  keep_N = n
+  n = 1
+  r = 1
+  
+  for Range_N in range(0, keep_N):
+    n *= Range_N
+  
+  for Range_R in range(0, a):
+    r *= Range_R
+  
+  ans = n/r
+  
+  return ans

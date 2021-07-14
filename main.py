b863dedfb9e1590e36eb61472b49b15a0a19da4a
kerorin kitakitsune <66837061+KerorinNorthFox@users.noreply.github.com>
2021-07-14 20:31:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-import permutation_func as pf
+import permutation_module as pf
 
-n = input("nを入力してください>>")
-r = input("rを入力してください>>")
+while True:
+    n = input(">>nを入力してください:")
+    r = input(">>rを入力してください:")
+    
+    try:
+        n = int(n)
+        r = int(r)
 
-n = int(n)
-r = int(r)
+        ans = pf.permutation(n, r)
+    except:
+        print(">>整数を入力してください")
+        continue
+    
+    keep_R = r
+    r = 1
 
-ans = pf.permutation(n, r)
+    for Range_R in range(1, keep_R+1):
+        r *= Range_R
 
-keep_R = r
-r = 1
-
-for Range_R in range(1, keep_R+1):
-    r *= Range_R
-
-fans = ans/r
-
-print(f">>組み合わせ:{fans}")
+    fans = ans/r
+    print(f">>組み合わせ:{fans}")
+    
+    select = input(">>終了しますか?:[y/n]")
+    
+    if select == "y":
+        print(">>終了")
+        break
+    elif select == "n":
+        continue
+    else:
+        print(">>入力が間違っています")
+        print(">>終了します")
+        break
